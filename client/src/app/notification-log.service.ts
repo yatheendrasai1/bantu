6120e3dfc1527633538cf12ac89f4b69d1e9ec32
yatheendrasai1 <yatheendrasai1@gmail.com>
2024-07-21 22:35:44 +0530

    BANTU-4: Added environment File

--- client/src/app/notification-log.service.ts
+++ client/src/app/notification-log.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationLogService {
 
-  private apiUrl = 'http://localhost:5171/api/notificationlogs';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {
     console.log("kkkkkkkkkkkkkkkkkkkkk");
---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://bantu-listen.vercel.app/'
+};
