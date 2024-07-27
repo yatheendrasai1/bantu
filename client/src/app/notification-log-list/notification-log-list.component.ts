5d9a4bf81f469dde25a263ead2eedc284b24f245
yatheendrasai1 <yatheendrasai1@gmail.com>
2024-07-28 02:59:28 +0530

    BANTU-10 : Removed consoles, added proper headings

--- client/src/app/notification-log-list/notification-log-list.component.ts
+++ client/src/app/notification-log-list/notification-log-list.component.ts
@@ -9,16 +9,13 @@ import { NotificationLogService } from '../notification-log.service';
 
 export class NotificationLogListComponent implements OnInit {
   logs: any[] = [];
-
+  extractedData
   constructor(private notificationLogService: NotificationLogService) { 
-  console.log("%% ~ NotificationLogListComponent ~ logs:");
-
   }
 
   logsprocessor(data){
     let logs = [];
     data.forEach(element => {
-      console.log("-->", element);
       let logRec = element?.log;
       if(!logRec) return data;
       let displayLog = {
@@ -26,14 +23,49 @@ export class NotificationLogListComponent implements OnInit {
         title : logRec.title
       }
       logs.push(displayLog);
+      // this.extractedData = this.bankMessageParser(logRec.text);
     });
     return logs;
   }
 
+  bankMessageParser(transactionString){
+    // const transactionString = "HDFC Bank: Rs. 1.00 credited to a/c XXXXXX4037 on 27-07-24 by a/c linked to VPA kavya.dammala@axl (UPI Ref No 457577568249).";
+    if(!transactionString.startsWith("HDFC")){
+      return;
+    }
+    // Define regular expressions
+    const bankNameRegex = /^(.*?):/;
+    const moneyRegex = /Rs\. (\d+\.\d{2})/;
+    const transactionTypeRegex = /(credited|debited)/;
+    const accountNumberRegex = /a\/c (\w+)/;
+    const dateRegex = /on (\d{2}-\d{2}-\d{2})/;
+    const fromWhoRegex = /linked to VPA (.*?) \(UPI Ref No/;
+    const upiRefNumberRegex = /UPI Ref No (\d+)/;
+
+    // Extract data using regex
+    const bankName = (transactionString.match(bankNameRegex) || [])[1];
+    const money = (transactionString.match(moneyRegex) || [])[1];
+    const transactionType = (transactionString.match(transactionTypeRegex) || [])[1];
+    const accountNumber = (transactionString.match(accountNumberRegex) || [])[1];
+    const date = (transactionString.match(dateRegex) || [])[1];
+    const fromWho = (transactionString.match(fromWhoRegex) || [])[1];
+    const upiRefNumber = (transactionString.match(upiRefNumberRegex) || [])[1];
+
+    // Construct JSON object
+    const extractedData = {
+        "Bank Name": bankName,
+        "Money": money,
+        "Transaction Type": transactionType,
+        "Account Number": accountNumber,
+        "Date": date,
+        "From Whom": fromWho,
+        "UPI Reference Number": upiRefNumber
+    };
+    console.log(extractedData);
+  }
+
   ngOnInit(): void {
-    console.log("heyyyyyyyyyyyy")
     this.notificationLogService.getLogs().subscribe(data => {
-      console.log(data);
       let displayLogs = this.logsprocessor(data);
       this.logs = displayLogs;
     });
